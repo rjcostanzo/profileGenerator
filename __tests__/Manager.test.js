4e1a33c931ecb351edd6c8292ff842a621a5cd90
rjcostanzo <rjcostanzo@gmail.com>
2021-10-24 13:43:06 -0500

    removed some obsolete files and directories

--- __tests__/Manager.test.js
+++
@@ -1,7 +0,0 @@
-const manager = require("../__tests__/Manager.test.js")
-
-test('manager test', () => {
-    const manager = new Manager('health');
-  
-    expect(manager.role).toBe('manager');
-  });
